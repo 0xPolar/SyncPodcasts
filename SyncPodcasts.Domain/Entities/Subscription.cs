a97b9e1a88ca23995ac15abd2d30151c5bc9d360
Drew <oaklap@gmail.com>
2026-02-09 18:48:15 +0000

    create subscription model

---
+++ SyncPodcasts.Domain/Entities/Subscription.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SyncPodcast.Domain.Entities
+{
+    public class Subscription
+    {
+        public Guid ID { get; private set; }
+        public Guid UserID { get; private set; }
+        public Guid PodcastID { get; private set; }
+        public DateTime SubscribedAt { get; private set; }
+
+        public Subscription(Guid userId, Guid podcastId)
+        {
+            ID = Guid.NewGuid();
+            UserID = userId;
+            PodcastID = podcastId;
+            SubscribedAt = DateTime.UtcNow;
+        }
+
+        private Subscription() {}
+    }
+}
