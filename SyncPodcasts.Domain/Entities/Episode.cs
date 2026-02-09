766af9f02d991158335c803378de5f6b024a7f7f
Drew <oaklap@gmail.com>
2026-02-09 18:28:53 +0000

    episode model created

---
+++ SyncPodcasts.Domain/Entities/Episode.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SyncPodcast.Domain.Entities
+{
+    public class Episode
+    {
+        public Guid ID { get; set; }
+        public Guid PodcastID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Uri AudioUrl { get; set; }
+        public TimeSpan Duration { get; set; }
+        public DateTime PublishedAt { get; set; }
+
+        public Episode(Guid podcastId, string title, string description,
+            Uri audioUrl, TimeSpan duration, DateTime publishedAt)
+        {
+            ID = Guid.NewGuid();
+            PodcastID = podcastId;
+            Title = title;
+            AudioUrl = audioUrl;
+            Description = description;
+            Duration = duration;
+            PublishedAt = publishedAt;
+
+        }
+        private Episode() { }
+    }
+}
---
+++ SyncPodcasts.Domain/Entities/Poscast.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SyncPodcast.Domain.Entities
+{
+    public class Poscast
+    {
+        private readonly List<Episode>
+    }
+}
