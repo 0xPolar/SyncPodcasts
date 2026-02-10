caeea8fcf4da2ff1d4fb8998d9131611e0192ebe
Drew <oaklap@gmail.com>
2026-02-10 16:34:12 +0000

    created cqrs queries

---
+++ SyncPodcast.Application/CQRS/Query.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SyncPodcast.Application.CQRS
+{
+    public record GetLibraryQuery(Guid UserId) : IRequest<List<LibraryDTO>>;
+
+    public record SearchPodcastQuery(string Query, int Page, int PageSize) : IRequest<List<PodcastSearchResultDTO>>;
+    public record GetPodcastDetailsQuery(Guid PodcastId) : IRequest<PodcastDetailsDTO>;
+    public record GetPlaybackProgressQuery(Guid UserId, Guid EpisodeId) : IRequest<PlaybackProgressDTO>;
+}
