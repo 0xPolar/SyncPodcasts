dc724fabbca6f60b3a523cff65ea469372490fd3
Drew <oaklap@gmail.com>
2026-02-13 20:14:54 +0000

    added episode db configuation

---
+++ SyncPoscast.Infrastructure/Configurations/EpisodeConfiguation.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SyncPodcast.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SyncPoscast.Infrastructure.Configurations;
+
+public class EpisodeConfiguation : IEntityTypeConfiguration<Episode>
+{
+    public void Configure(EntityTypeBuilder<Episode> builder)
+    {
+        builder.HasKey(e => e.ID);
+
+        builder.Property(e => e.Title)
+            .IsRequired()
+            .HasMaxLength(200);
+
+        builder.Property(e => e.MediaUrl)
+            .HasConversion(
+                v => v.ToString(),
+                v => new Uri(v))
+            .HasMaxLength(2000)
+            .IsRequired();
+
+        builder.HasIndex(e => e.PodcastID);
+    }
+}
