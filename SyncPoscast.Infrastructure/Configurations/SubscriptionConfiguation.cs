912bd3580e78e746c13c05502f443aa2140d8da7
Drew <oaklap@gmail.com>
2026-02-13 20:18:04 +0000

    added subcription db config

---
+++ SyncPoscast.Infrastructure/Configurations/SubscriptionConfiguation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SyncPodcast.Domain.Entities;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SyncPoscast.Infrastructure.Configurations;
+
+public class SubscriptionConfiguation : IEntityTypeConfiguration<Subscription>
+{
+    public void Configure(EntityTypeBuilder<Subscription> builder)
+    {
+        builder.HasKey(s => s.ID);
+
+        // Composite unique constraint to prevent duplicate subscriptions for the same user and podcast
+        builder.HasIndex(s => new { s.UserID, s.PodcastID }).IsUnique();
+
+        // Index on UserID for faster lookups of a user's subscriptions
+        builder.HasIndex(s => s.UserID);
+    }
+}
