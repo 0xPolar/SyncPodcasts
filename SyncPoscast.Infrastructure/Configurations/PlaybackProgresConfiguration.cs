4cb89cded55f0eb29b2d787e3f1d99521e6b92d1
Drew <oaklap@gmail.com>
2026-02-13 20:19:05 +0000

    added user playback progress db config

---
+++ SyncPoscast.Infrastructure/Configurations/PlaybackProgresConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SyncPodcast.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SyncPoscast.Infrastructure.Configurations;
+
+public class PlaybackProgresConfiguration : IEntityTypeConfiguration<PlaybackProgress>
+{
+    public void Configure(EntityTypeBuilder<PlaybackProgress> builder)
+    {
+        builder.HasKey(p => p.ID);
+
+        // Composite unique constraint to ensure one progress record per user and episode
+        builder.HasIndex(p => new { p.UserID, p.EpisodeID }).IsUnique();
+
+        // Index on UserID for faster lookups of a user's playback progress
+        builder.HasIndex(p => p.UserID);
+    }
+}
