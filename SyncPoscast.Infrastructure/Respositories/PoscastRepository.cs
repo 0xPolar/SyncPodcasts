8ea7aeb9b8d9a0a4a0cea1a567bb37fd7661b99f
Drew <oaklap@gmail.com>
2026-02-13 21:46:40 +0000

    added podcast db repo

---
+++ SyncPoscast.Infrastructure/Respositories/PoscastRepository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.EntityFrameworkCore;
+using SyncPodcast.Domain.Entities;
+using SyncPodcast.Domain.Interfaces;
+using SyncPoscast.Infrastructure.Persistence;
+
+namespace SyncPoscast.Infrastructure.Respositories;
+
+public class PoscastRepository : IPodcastRepository
+{
+    private readonly SyncPodcastDbContext _db;
+    public PoscastRepository(SyncPodcastDbContext db) => _db = db;
+
+    public async Task<Podcast?> GetPodcastByIdAsync(Guid id, CancellationToken ct)
+    {
+        return await _db.Podcasts
+            .Include(p => p.Episodes) // Include episodes when fetching the podcast
+            .FirstOrDefaultAsync(p => p.ID == id, ct);
+    }
+
+    public async Task<Podcast?> GetByFeedUrlAsync(Uri feedUrl, CancellationToken ct)
+    {
+        // used when checking if podcast with url exists no need to include episodes
+        return await _db.Podcasts
+            .FirstOrDefaultAsync(p => p.FeedUrl == feedUrl, ct);
+    }
+
+    public async Task<Episode?> GetEpisodeByIdAsync(Guid id, CancellationToken ct)
+    {
+        return await _db.Episodes
+            .FirstOrDefaultAsync(e => e.ID == id, ct);
+    }
+
+    public async Task AddAsync(Podcast podcast, CancellationToken ct)
+    {
+        _db.Add(podcast);
+
+        await _db.SaveChangesAsync(ct);
+    }
+
+    public async Task UpdateAsync(Podcast podcast, CancellationToken ct)
+    {
+        _db.Update(podcast);
+
+        await _db.SaveChangesAsync(ct);
+    }
+
+    public async Task DeleteAsync(Guid id, CancellationToken ct)
+    {
+        Podcast? podcast = await _db.Podcasts.FirstOrDefaultAsync(p => p.ID == id, ct);
+        if (podcast is not null)
+        {
+            _db.Remove(podcast);
+
+            await _db.SaveChangesAsync(ct);
+        }
+    }
+}
