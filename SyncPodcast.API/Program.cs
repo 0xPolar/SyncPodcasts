7f2717dbb09682e00e8a05e3e3a2bcf3f643d474
Drew <oaklap@gmail.com>
2026-02-18 20:31:58 +0000

    configured main program.cs for api layer

--- SyncPodcast.API/Program.cs
+++ SyncPodcast.API/Program.cs
@@ -10,6 +10,48 @@ var builder = WebApplication.CreateBuilder(args);
 
 /// Service Regestration
 builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
+// Jwt Authentication
+var jwtSettings = builder.Configuration.GetSection("Jwt").Get<JWTSettings>()!;
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtSettings.Issuer,
+            ValidAudience = jwtSettings.Audience,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(jwtSettings.Secret)),
+            ClockSkew = TimeSpan.Zero // Optional: reduce default clock skew
+        };
+    });
+builder.Services.AddAuthorization();
+
+var app = builder.Build();
+
+// Http Pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+// Custom exception handling middleware
+app.UseMiddleware<ExceptionHandlingMiddleware>();
+
+// Authentication & Authorization
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
