633367364b0bc0ac16386b7f474c53e8c410f8f0
Drew <oaklap@gmail.com>
2026-02-19 23:02:19 +0000

    added library controller

---
+++ SyncPodcast.API/Controllers/LibraryController.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SyncPodcast.Application.CQRS;
+using SyncPodcast.API.Extensions;
+
+namespace SyncPodcast.API.Controllers;
+
+[ApiController]
+[Route("api/v1/library")]
+[Authorize]
+public class LibraryController : ControllerBase
+{
+    private readonly ISender _mediator;
+    public LibraryController(ISender mediator)
+    {
+        _mediator = mediator;
+    }
+    // GET: api/v1/library
+    [HttpGet("api/v1/library")]
+    public async Task<IActionResult> GetLibrary()
+    {
+        var userId = User.GetUserId();
+        var result = await _mediator.Send(new GetLibraryQuery(userId));
+        return Ok(result);
+    }
+}
