2ab31c288fdc84c85070849fa1a96e323770260a
Drew <oaklap@gmail.com>
2026-02-24 21:53:35 +0000

    set refresh token test

--- SyncPodcast.Domain.Tests/Entities/UserTests.cs
+++ SyncPodcast.Domain.Tests/Entities/UserTests.cs
@@ -10,10 +10,10 @@ public class  UserTests
     [Fact]
     public void Constructor_WithValidArgs_SetsallProperties()
     {
-        var id = Guid.NewGuid();
-        var now = DateTime.UtcNow;
+        Guid id = Guid.NewGuid();
+        DateTime now = DateTime.UtcNow;
 
-        var user = new User(id, "john21", "john@example.com", "hash123", now);
+        User user = new User(id, "john21", "john@example.com", "hash123", now);
 
         Assert.Equal(id, user.ID);
         Assert.Equal("alice", user.Username);
@@ -22,6 +22,27 @@ public class  UserTests
         Assert.Equal(now, user.CreatedAt);
         Assert.Null(user.RefreshToken);
         Assert.Null(user.RefreshTokenExpiry);
+    }
+
+    [Fact]
+    public void SetRefreshToken_WithValidArgs_SetsTokenAndExpiry()
+    {
+        Guid id = Guid.NewGuid();
+        string token = Guid.NewGuid().ToString();
+        DateTime now = DateTime.UtcNow;
+
+        User user = new User(id, "john21", "john@example.com", "hash123", now);
+
+        user.SetRefreshToken(token, now);
+
+        Assert.Equal(token, user.RefreshToken);
+        Assert.Equal(now, user.RefreshTokenExpiry);
+    }
+
+    [Fact]
+    public void ValidateRefreshToken_WhenTokenMatches_DoesNotThrow()
+    {
+
     }
     
 }
